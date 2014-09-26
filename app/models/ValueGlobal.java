f85306466bfe918a68dc4b89228d2ee4bd246b39
seameteo <dragonbird.tonbo@gmail.com>
2014-09-26 15:26:19 +0900

    postするたびに色が変わる

--- app/models/ValueGlobal.java
+++ app/models/ValueGlobal.java
@@ -1,8 +1,11 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import models.apis.ApiColorColor;
 import play.db.ebean.*;
 
 @Entity
@@ -12,9 +15,55 @@ public class ValueGlobal extends Model {
 	@Id
 	public Long id;
 	
-	public String state;
+	public String state = "Natural";
     
-    public int backColor;
-    public int TitleColor;
+    public int backColor  = 0xFFFFFF;
+    public int TitleColor = 0x000000;
     
+    public String strBCol() {
+    	return String.format("#%06X", backColor);
+    }
+    public String strTCol() {
+    	return String.format("#%06X", TitleColor);
+    }
+
+    public void changeColors(int auraCol, String emo) {
+    	
+    	backColor = Color.blendWith(auraCol, backColor, 0.1).encode();
+    	
+    	if (emo != null && emo != "") {
+    		// Color API
+        	ApiColorColor apiColor = new ApiColorColor(emo);
+    		int apiCRes = apiColor.Send();
+    		if (apiCRes == 0) {
+    			// OK
+    			int colnum = apiColor.colors.size();
+    			int tcol = apiColor.colors.get(colnum-1);
+    			TitleColor = Color.blendWith(tcol, TitleColor, 0.7).encode();
+    		}
+    	}
+    		
+    }
+    
+	/**
+	 * The Finder
+	 */
+    public static final Finder<Long,ValueGlobal> find =
+    	new Finder<Long,ValueGlobal>(Long.class, ValueGlobal.class);
+    
+    private static ValueGlobal vals;
+    
+    public static ValueGlobal getGVal() {
+    	if (vals != null) return vals;
+    	
+    	List<ValueGlobal> l = find.all();
+    	if (l.size() == 0) {
+    		ValueGlobal newnew = new ValueGlobal();
+    		l.add(newnew);
+    		newnew.save();
+    	}
+    	vals = l.get(0);
+    	
+    	return vals;
+    }
 }
